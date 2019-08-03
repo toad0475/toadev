40d21f602acfc9792a4791b94eb532eac2fe5154
toad0475 <toad0475@gmail.com>
2019-08-03 16:30:01 +0900

    update With Statement in wavefileClassifier

--- Toad_Dev/WavefileClassifier.py
+++ Toad_Dev/WavefileClassifier.py
@@ -9,23 +9,23 @@ def waveClassifier():
     files = []
 
     # src 파일 경로 리스트 만들기
-    for r, d, f in os.walk(srcDir):
+    for r,d,f in os.walk(srcDir):
         #r=root, d=directories, f=files
         files += [os.path.join(r,file) for file in f if '.wav' in file]
     
     # stereo, mono 각각의  폴더로 분류 작업
     for wavefile in files:
-        wf = wave.Wave_read(wavefile)
         subPath = getSubPath(srcDir, wavefile)
-        if wf.getnchannels()==2:
-            if not os.path.isdir(os.path.dirname(stereoTargetDir+subPath)):
-                os.makedirs(os.path.dirname(stereoTargetDir+subPath))
-            shutil.copy(wavefile, stereoTargetDir+subPath)
-        elif wf.getnchannels()==1:
-            if not os.path.isdir(os.path.dirname(monoTargetDir+subPath)):
-                os.makedirs(os.path.dirname(monoTargetDir+subPath))
-            shutil.copy(wavefile, monoTargetDir+subPath)
-        wf.close()
+
+        with wave.Wave_read(wavefile) as wf:
+            if wf.getnchannels()==2:
+                if not os.path.isdir(os.path.dirname(stereoTargetDir+subPath)):
+                    os.makedirs(os.path.dirname(stereoTargetDir+subPath))
+                shutil.copy(wavefile, stereoTargetDir+subPath)
+            elif wf.getnchannels()==1:
+                if not os.path.isdir(os.path.dirname(monoTargetDir+subPath)):
+                    os.makedirs(os.path.dirname(monoTargetDir+subPath))
+                shutil.copy(wavefile, monoTargetDir+subPath)
 
 # 소스 경로와 목적 경로에서 겹치치 않는 하위 경로만 가져오기
 def getSubPath(src, target):
