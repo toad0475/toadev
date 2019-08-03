bdebcf6451c52c1c3d46c4d47827c7df104296eb
toadwave <toad0475@gmail.com>
2019-08-03 21:42:06 +0900

    Modify indent

--- CodingTest/SelectionSort.py
+++ CodingTest/SelectionSort.py
@@ -1,19 +1,14 @@
 #python3
+#선택정렬 알고리즘 구현
 
 def SelectionSort(A, n):
-    for i in range(n-1):
-        least = A[i]
-        
-        for j in range(i+1, n):
-            if least>A[j]:
-                least = A[j]
-                A[i],A[j] = A[j],A[i]
-
-    return A
-    
-    
-
-
+	for i in range(n-1):
+		least = A[i]
+		for j in range(i+1, n):
+			if least>A[j]:
+				least = A[j]
+				A[i],A[j] = A[j],A[i]
+	return A
 
 A = [4, 7, 2, 6, 8]
 print(SelectionSort(A,len(A)))
