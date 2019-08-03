c81d61cfdb3da52660ad6c21f3279245859a9fb8
toad0475 <toad0475@gmail.com>
2019-08-03 16:40:07 +0900

    Uploaded

---
+++ CodingTest/SelectionSort.py
@@ -0,0 +1,19 @@
+#python3
+
+def SelectionSort(A, n):
+    for i in range(n-1):
+        least = A[i]
+        
+        for j in range(i+1, n):
+            if least>A[j]:
+                least = A[j]
+                A[i],A[j] = A[j],A[i]
+
+    return A
+    
+    
+
+
+
+A = [4, 7, 2, 6, 8]
+print(SelectionSort(A,len(A)))
