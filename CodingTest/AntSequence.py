8b839de8bf33056f51a773dfe79a88a786dd38ad
toadwave <toad0475@gmail.com>
2019-08-04 17:01:16 +0900

    updated

--- CodingTest/AntSequence.py
+++ CodingTest/AntSequence.py
@@ -1,11 +1,17 @@
-# 개미수열 문제
-# null 출제 문제
+# 개미수열(Ant Sequence) 문제
 
 def antSeq(n):
 	if n==1:
 		return '1'
 	else:
-		
-		antSeq(n-1)+len
-	return 
+		return antSeq(n-1)
 
+def getContNum(s):
+	n = 1
+	for i in range(len(s)-2):
+		if s[i]==s[i+1]:
+			n += 1
+		else:
+			str(n)
+			n=1
+			 
