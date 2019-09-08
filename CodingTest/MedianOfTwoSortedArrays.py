71c084d4ec9a11dd64b3b1aa25196c680c723383
toad0475 <toad0475@gmail.com>
2019-09-09 08:42:23 +0900

    modified

--- CodingTest/MedianOfTwoSortedArrays.py
+++ CodingTest/MedianOfTwoSortedArrays.py
@@ -18,49 +18,55 @@ nums2 = [3, 4]
 
 The median is (2 + 3)/2 = 2.5
 '''
+import bisect
 
 def findMedianSortedArrays(nums1, nums2):
-	n = len(nums1)
-	m = len(nums2)
-	
-	nums1_head = 0
-	nums1_tail = n-1
-	
-	nums2_head = 0
-	nums2_tail = m-1
-	
-	# get median in array
-	if n%2:
-		nums1_med = float(nums1[n//2])
-	else:
-		nums1_med = (nums1[n//2]+nums1[n//2-1])/2
-	if m%2:
-		nums2_med = float(nums2[m//2])
-	else:
-		nums2_med = (nums2[n//2]+nums2[n//2-1])/2
-	
-	if nums1_med == nums2_med:
-		return nums1_med
-	elif nums1_med > nums2_med:
-		nums1_head = nums1_med
-		return findMedianSortedArrays(
-			nums1[:n//2],nums2[m//2:])
-	else:
-		return findMedianSortedArrays(
-			nums1[n//2:],nums2[:m//2+1])
-	
 
-print(solution(
-	[5,8,9,15],
-	[2,5,6]
-	))
+    if len(nums1)<len(nums2):
+        nums1, nums2 = nums2, nums1
+    n = len(nums1)
+    m = len(nums2)
+    
+    # head = 0
+    # tail = n-1
+    # mid = (n+m)/2 
+    
+    nums1_med = getMedianSortedArray(nums1, n)
+    nums2_med = getMedianSortedArray(nums2, m)
 
+    if m == 1:
+        insert(nums1, nums2[0])
+        return getMedianSortedArray(nums1, n+1)
+
+    if nums1_med == nums2_med:
+        return nums1_med
+    elif nums1_med > nums2_med:
+        return findMedianSortedArrays(nums1[m//2:n//2],nums2[m//2:])
+    else:
+	    return findMedianSortedArrays(nums1[m//2:],nums2[:m//2+1])
+
+def getMedianSortedArray(array, length):
+    if length % 2:
+        return float(array[length//2])
+    else:
+        return (array[length//2]+array[length//2-1])/2 
+
+def insert(array, n):
+    bisect.insort(array, n)
+    return array
+
+nums1 = [3,4,6,7,8,9,12,13,15,16,18,20,21,22,23]
+nums2 = [5,10,11,14,17,24,25]
+print(findMedianSortedArrays(nums1, nums2))
+
+
+'''
 
 # 두번째 솔루션
 
 def fmsa(nums1, nums2):
     # 항상 두번재 배열 길이가 길거나 같도록 유지
-    if len(nums1)>len(nums2):
+    if len(nums1) < len(nums2):
         nums1, nums2 = nums2, nums1
     nums1_len = len(nums1)
     nums2_len = len(nums2)
@@ -77,7 +83,7 @@ def fmsa(nums1, nums2):
 
     while (head <= tail):
         i = int((head + tail) / 2)  # i = nums1 의 길이 / 2 
-        j = int(mid - i)         # j = (두 개 합친 총 길이 / 2) - i
+        j = int(mid - i)            # j = (두 개 합친 총 길이 / 2) - i
         
         if i < nums1_len and nums2[j-1] > nums1[i]:
             head = i + 1
@@ -110,8 +116,8 @@ def fmsa(nums1, nums2):
     return -1
 
 
-nums1 = [4,6]
-nums2 = [5]
+nums1 = [4,5]
+nums2 = [6]
 print(fmsa(nums1,nums2))
 
-	
+'''	
