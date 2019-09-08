fc64195cc8dba26c8116cedb46d57e4689cb18d9
toadwave <toad0475@gmail.com>
2019-09-08 15:01:10 +0900

    Update MedianOfTwoSortedArrays.py

--- CodingTest/MedianOfTwoSortedArrays.py
+++ CodingTest/MedianOfTwoSortedArrays.py
@@ -54,4 +54,64 @@ print(solution(
 	[5,8,9,15],
 	[2,5,6]
 	))
+
+
+# 두번째 솔루션
+
+def fmsa(nums1, nums2):
+    # 항상 두번재 배열 길이가 길거나 같도록 유지
+    if len(nums1)>len(nums2):
+        nums1, nums2 = nums2, nums1
+    nums1_len = len(nums1)
+    nums2_len = len(nums2)
+    
+    # 둘 중 길이가 긴 배열의 머리와 꼬리
+    head = 0
+    tail = nums1_len
+
+    # 1을 더하는 이유는 두 배열 길이의 합이 짝수던진 홀수던지 상관없이 중간 값을 나중에 int로 끊어주기 위한 장치
+    mid = (nums1_len + nums2_len + 1) / 2
+
+    i = 0
+    j = 0
+
+    while (head <= tail):
+        i = int((head + tail) / 2)  # i = nums1 의 길이 / 2 
+        j = int(mid - i)         # j = (두 개 합친 총 길이 / 2) - i
+        
+        if i < nums1_len and nums2[j-1] > nums1[i]:
+            head = i + 1
+        
+        elif i > 0 and nums1[i-1] > nums2[j]:
+            tail = i - 1
+        else:
+            maxleft = 0
+            
+            if i == 0:
+                maxleft = nums2[j-1]
+            elif j == 0:
+                maxleft = nums1[i-1]
+            else:
+                maxleft = int(max(nums1[i-1], nums2[j-1]))
+
+            if (nums1_len + nums2_len)%2:
+                return maxleft
+            
+            minright = 0
+            
+            if i == nums1_len:
+                minright = nums2[j]
+            elif j == nums2_len:
+                minright = nums1[i]
+            else:
+                minright = int(min(nums1[i], nums2[j]))
+            
+            return (maxleft + minright) / 2
+    return -1
+
+
+nums1 = [4,6]
+nums2 = [5]
+print(fmsa(nums1,nums2))
+
 	
