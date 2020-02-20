dfea61b6972ec4f88a1cde34c33336b75bf6f892
toadwave <toad0475@gmail.com>
2020-02-20 21:37:55 +0900

    Updated minor changes

--- Toad_Dev/FindWprojFile.bat
+++ Toad_Dev/FindWprojFile.bat
@@ -1,8 +1,8 @@
 @ECHO OFF
 
-@ECHO TargetPath: %1
-@ECHO CurrentDir: %cd%
-@ECHO BATDir: %~dp0
+@ECHO TARGETDIR: %1
+@ECHO CURRENTDIR: %cd%
+@ECHO BATCHDIR: %~dp0
 
 SET CLIPATH=%cd%\wwisecli.exe
 @ECHO CLIPATH: %CLIPATH%
@@ -12,14 +12,14 @@ SET CLIPATH=%cd%\wwisecli.exe
 @REM 배치파일이 있는 디렉토리로 경로 이동
 CHDIR /d %~dp0
 
-@REM 검사를 수행 할 확장자를 설정
+@REM 찾고자 하는 파일의 확장자를 설정
 SET TARGET_FILEX=wproj
 
 @REM 배치파일이 있는 위치로부터 몇 번째 상위레벨까지 탐색할지 설정
-SET /A LOOPCOUNT=4
+SET /a LOOPCOUNT=4
 
 :LOOP
-FOR /R %%f IN (*.%TARGET_FILEX%) DO (
+FOR /r %%f IN (*.%TARGET_FILEX%) DO (
     IF EXIST %%f (
         SET WWISEPROJPATH=%%f
         GOTO SHOWPATH 
@@ -29,7 +29,7 @@ FOR /R %%f IN (*.%TARGET_FILEX%) DO (
 IF %LOOPCOUNT% == 0 (
     GOTO DOESNTEXIST
 ) ELSE (
-    SET /A LOOPCOUNT-= 1
+    SET /a LOOPCOUNT-= 1
     CHDIR ..
     GOTO LOOP
 )
